957191b4fd15552e7050feb46438ea480fe381b8
Samuel Laroche <samuellaroche@live.ca>
2019-02-09 10:31:07 -0500

    created angular app

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var url = require('url');
 var querystring = require('querystring');
 var fs = require('fs');
 
-var route = require('./route');
+var route = require('./server/route');
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,29 +13,11 @@ app.use(bodyParser.json());
 const $PORT = process.env.port || 8080;
 
 //To use static assets directories
-app.use(express.static('css'));
-app.use(express.static('js'));
-app.use(express.static('images'));
-
-//Template engine
-app.engine('html', function (filePath, options, callback) { // define the template engine
-  fs.readFile(filePath, function (err, content) {
-    if (err) return callback(err)
-    // this is an extremely simple template engine
-    var rendered = content.toString()
-    .replace('#report#', options.report)
-    .replace('#message#', options.message)
-    .replace('#script#', options.script)
-    return callback(null, rendered)
-  })
-})
-
-app.set('views', './views') // specify the views directory
-app.set('view engine', 'html') // register the template engine
+app.use(express.static('dist'));
 
 app.get('/', function(req,res){
 
-    fs.readFile('index.html', 'utf8', function(err,content){
+    fs.readFile('dist/index.html', 'utf8', function(err,content){
         res.send(content);
     })
 })
